5a2e638f38cc12113db0d667ff8fe35866ac3bd3
krzysiek95 <karskikrzysztof95@gmail.com>
2021-07-22 18:18:45 +0200

    Pierwsze wyslanie aplikacji

---
+++ Psychotechnika test/Form1.Designer.vb
@@ -0,0 +1,256 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Formularz przesłania metodę dispose, aby wyczyścić listę składników.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wymagane przez Projektanta formularzy systemu Windows
+    Private components As System.ComponentModel.IContainer
+
+    'UWAGA: następująca procedura jest wymagana przez Projektanta formularzy systemu Windows
+    'Możesz to modyfikować, używając Projektanta formularzy systemu Windows. 
+    'Nie należy modyfikować za pomocą edytora kodu.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer3 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer4 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer5 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer6 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer7 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer8 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 72.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.Label1.Location = New System.Drawing.Point(8, 8)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(536, 112)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Label1"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label2
+        '
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 72.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.Label2.Location = New System.Drawing.Point(8, 120)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(536, 120)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Label2"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label3
+        '
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 72.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.Label3.Location = New System.Drawing.Point(0, 240)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(544, 112)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Label3"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'Timer2
+        '
+        Me.Timer2.Interval = 1
+        '
+        'Timer3
+        '
+        Me.Timer3.Interval = 1000
+        '
+        'Timer4
+        '
+        Me.Timer4.Interval = 1
+        '
+        'Timer5
+        '
+        Me.Timer5.Interval = 1000
+        '
+        'Timer6
+        '
+        Me.Timer6.Interval = 1
+        '
+        'Timer7
+        '
+        Me.Timer7.Interval = 1000
+        '
+        'Timer8
+        '
+        Me.Timer8.Interval = 1
+        '
+        'Label4
+        '
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.Label4.Location = New System.Drawing.Point(552, 8)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(184, 23)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Próba"
+        Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label5
+        '
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.Label5.Location = New System.Drawing.Point(552, 32)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(184, 23)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Label5"
+        Me.Label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label6
+        '
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.Label6.Location = New System.Drawing.Point(552, 88)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(184, 23)
+        Me.Label6.TabIndex = 6
+        Me.Label6.Text = "Label6"
+        Me.Label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label7
+        '
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.Label7.Location = New System.Drawing.Point(552, 64)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(184, 23)
+        Me.Label7.TabIndex = 5
+        Me.Label7.Text = "Średni czas reakcji"
+        Me.Label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label8
+        '
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.Label8.Location = New System.Drawing.Point(552, 144)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(184, 23)
+        Me.Label8.TabIndex = 8
+        Me.Label8.Text = "Label8"
+        Me.Label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label9
+        '
+        Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.Label9.Location = New System.Drawing.Point(552, 120)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(184, 23)
+        Me.Label9.TabIndex = 7
+        Me.Label9.Text = "Najmniejszy czas reakcji"
+        Me.Label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label10
+        '
+        Me.Label10.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.Label10.Location = New System.Drawing.Point(552, 200)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(184, 23)
+        Me.Label10.TabIndex = 10
+        Me.Label10.Text = "Label10"
+        Me.Label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label11
+        '
+        Me.Label11.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.Label11.Location = New System.Drawing.Point(552, 176)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(184, 23)
+        Me.Label11.TabIndex = 9
+        Me.Label11.Text = "Największy czas reakcji"
+        Me.Label11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label12
+        '
+        Me.Label12.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.Label12.Location = New System.Drawing.Point(552, 256)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(184, 23)
+        Me.Label12.TabIndex = 12
+        Me.Label12.Text = "Label12"
+        Me.Label12.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label13
+        '
+        Me.Label13.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.Label13.Location = New System.Drawing.Point(552, 232)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(184, 23)
+        Me.Label13.TabIndex = 11
+        Me.Label13.Text = "Błędy"
+        Me.Label13.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(737, 353)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents Timer3 As Timer
+    Friend WithEvents Timer4 As Timer
+    Friend WithEvents Timer5 As Timer
+    Friend WithEvents Timer6 As Timer
+    Friend WithEvents Timer7 As Timer
+    Friend WithEvents Timer8 As Timer
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label13 As Label
+End Class
---
+++ Psychotechnika test/Form1.vb
@@ -0,0 +1,205 @@
+﻿Public Class Form1
+    Dim inte As Integer = 0
+    Dim proba As Integer = 0
+    Dim czasReakcjiMin As Integer = 100000
+    Dim czasReakcjiMax As Integer = 0
+    Dim czasReakcjiAll As Integer = 0
+    Dim iloscProb As Integer = -1
+    Dim bledy As Integer = 0
+
+    Dim dtStartTime As Date
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Randomize()
+
+        iloscProb = InputBox("Wprowadź liczbę prób", "Ilość prób")
+
+        Label1.Text = ""
+        Label2.Text = ""
+        Label3.Text = ""
+
+        Label5.Text = 1
+        Label6.Text = 0 & "ms"
+        Label8.Text = 0 & "ms"
+        Label10.Text = 0 & "ms"
+        Label12.Text = 0
+
+        Label1.BackColor = Color.White
+        Label2.BackColor = Color.White
+        Label3.BackColor = Color.White
+
+        If MessageBox.Show("Start", "Start") = DialogResult.OK Then
+            Timer1.Start()
+            Timer3.Start()
+            Timer5.Start()
+        End If
+    End Sub
+
+    Sub odswiez()
+        Label6.Text = Math.Round((czasReakcjiAll / (proba)), 3) & "ms"
+        Label8.Text = czasReakcjiMin & "ms"
+        Label10.Text = czasReakcjiMax & "ms"
+        Label12.Text = bledy
+
+        If proba >= iloscProb Then
+            Timer1.Stop()
+            Timer3.Stop()
+            Timer5.Stop()
+            MsgBox("Koniec. Twoje wyniki są w oknie.")
+        Else
+            Label5.Text = proba + 1
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Dim los As Integer = CInt(Int((100 * Rnd()) + 1))
+
+        If los Mod 3 = 0 Then
+            dtStartTime = Now
+
+            Timer2.Start()
+            Timer1.Stop()
+            Timer3.Stop()
+            Timer5.Stop()
+
+            Label1.BackColor = Color.Red
+        End If
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        Dim tsDiff As TimeSpan = Now.Subtract(dtStartTime)
+        Label1.Text = tsDiff.TotalMilliseconds & "ms"
+        inte = tsDiff.TotalMilliseconds
+    End Sub
+
+    Private Sub Timer3_Tick(sender As Object, e As EventArgs) Handles Timer3.Tick
+        Dim los As Integer = CInt(Int((100 * Rnd()) + 1))
+
+        If los Mod 3 = 0 Then
+            dtStartTime = Now
+
+            Timer4.Start()
+            Timer1.Stop()
+            Timer3.Stop()
+            Timer5.Stop()
+
+            Label2.BackColor = Color.Green
+        End If
+    End Sub
+
+    Private Sub Timer4_Tick(sender As Object, e As EventArgs) Handles Timer4.Tick
+        Dim tsDiff As TimeSpan = Now.Subtract(dtStartTime)
+        Label2.Text = tsDiff.TotalMilliseconds & "ms"
+        inte = tsDiff.TotalMilliseconds
+    End Sub
+
+    Private Sub Timer5_Tick(sender As Object, e As EventArgs) Handles Timer5.Tick
+        Dim los As Integer = CInt(Int((100 * Rnd()) + 1))
+
+        If los Mod 3 = 0 Then
+            dtStartTime = Now
+
+            My.Computer.Audio.Play(My.Resources.aaa, AudioPlayMode.Background)
+
+            Timer6.Start()
+            Timer1.Stop()
+            Timer3.Stop()
+            Timer5.Stop()
+        End If
+    End Sub
+
+    Private Sub Timer6_Tick(sender As Object, e As EventArgs) Handles Timer6.Tick
+        Dim tsDiff As TimeSpan = Now.Subtract(dtStartTime)
+        Label3.Text = tsDiff.TotalMilliseconds & "ms"
+        inte = tsDiff.TotalMilliseconds
+    End Sub
+
+    Private Sub Form1_KeyDown(sender As Object, e As System.Windows.Forms.KeyEventArgs) Handles MyBase.KeyDown
+        If Timer2.Enabled Then
+            If e.KeyCode = Keys.Left Then
+                Timer2.Stop()
+                Threading.Thread.Sleep(2000)
+                Label1.Text = ""
+                Label1.BackColor = Color.White
+                czasReakcjiAll = czasReakcjiAll + inte
+
+                If inte < czasReakcjiMin Then
+                    czasReakcjiMin = inte
+                End If
+
+                If inte > czasReakcjiMax Then
+                    czasReakcjiMax = inte
+                End If
+
+                inte = 0
+                Timer1.Start()
+                Timer3.Start()
+                Timer5.Start()
+                proba = proba + 1
+                odswiez()
+            Else
+                My.Computer.Audio.Play(My.Resources.blad, AudioPlayMode.Background)
+                bledy = bledy + 1
+                Label12.Text = bledy
+            End If
+        End If
+
+        If Timer4.Enabled Then
+            If e.KeyCode = Keys.Right Then
+                Timer4.Stop()
+                Threading.Thread.Sleep(2000)
+                Label2.Text = ""
+                Label2.BackColor = Color.White
+                czasReakcjiAll = czasReakcjiAll + inte
+
+                If inte < czasReakcjiMin Then
+                    czasReakcjiMin = inte
+                End If
+
+                If inte > czasReakcjiMax Then
+                    czasReakcjiMax = inte
+                End If
+
+                inte = 0
+                Timer1.Start()
+                Timer3.Start()
+                Timer5.Start()
+                proba = proba + 1
+                odswiez()
+            Else
+                My.Computer.Audio.Play(My.Resources.blad, AudioPlayMode.Background)
+                bledy = bledy + 1
+                Label12.Text = bledy
+            End If
+        End If
+
+        If Timer6.Enabled Then
+            If e.KeyCode = Keys.Space Then
+                Timer6.Stop()
+                Threading.Thread.Sleep(2000)
+                Label3.Text = ""
+                Label3.BackColor = Color.White
+                czasReakcjiAll = czasReakcjiAll + inte
+
+                If inte < czasReakcjiMin Then
+                    czasReakcjiMin = inte
+                End If
+
+                If inte > czasReakcjiMax Then
+                    czasReakcjiMax = inte
+                End If
+
+                inte = 0
+                Timer1.Start()
+                Timer3.Start()
+                Timer5.Start()
+                proba = proba + 1
+                odswiez()
+            Else
+                My.Computer.Audio.Play(My.Resources.blad, AudioPlayMode.Background)
+                bledy = bledy + 1
+                Label12.Text = bledy
+            End If
+        End If
+    End Sub
+End Class
